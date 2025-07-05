855157810389a3ee82c9b50c3ef3db9fa15d099c
giselle-01 <giselleandrea027@gmail.com>
2025-07-04 19:32:41 -0500

    Feat: the styles were created

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import { alertError } from "./alert";
 
+//variables 
 const endopointProducts = "http://localhost:3000/products";
 const $nameProduct = document.getElementById("name_product");
 const $priceProduct = document.getElementById("price_product");
@@ -11,19 +12,19 @@ $form.addEventListener("submit", function (event) {
   createProduct();
 });
 
-//create a new product
+//function to create new products
 async function createProduct() {
-  const newProduct = {
+    const newProduct = {
     nameProduct: $nameProduct.value,
     priceProduct: $priceProduct.value,
     description: $description.value,
-  };
-
+    };
+};
   try {
-    let response = await fetch(endopointProducts, {
+    const response = await fetch(endopointProducts, {
       method: "POST",
       headers: {
-        "content-type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newProduct),
     });
@@ -35,19 +36,73 @@ async function createProduct() {
       throw new Error(response.statusText);
     } else {
       alert("Producto agregado con éxito");
-    }
+    };
+
   } catch (error) {
     console.log(error.message);
-  }
-}
+  };
 
-//delete a product
-async function deleteProduct() {
-    let response = await fetch(endopointProducts, {
-        method: "DELETE"
-        headers: {
-            "content-type": "aplication/json",
-        },
+
+//function to get all the products
+async function getProducts() {
+    try {
+        const response = await fetch(endopointProducts);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        };
+
+        const products = await response.json();
+        console.log(products);
         
-    })
-}
+    } catch (error) {
+    console.error(error.message);
+  };
+};
+
+//function to delete produts
+async function deleteProducts() {
+
+    getProducts(products)
+
+    try {
+        let response = await fetch(endopointProducts, {
+        method: "DELETE",
+        headers: {
+        "Content-Type": "application/json",
+        }
+    });
+    console.log(response)
+
+    if (!response.ok) {
+        alert("Lo sentimos, reintente más tarde");
+        throw new Error()
+    }
+    } catch (error) {
+        console.log(error.message);
+    };
+};
+
+//function to update products
+async function updateProducts() {
+    try {
+        let response = await fetch(endopointProducts, {
+        method: "PUT",
+        headers: {
+        "Content-Type": "application/json",
+        }
+    });
+    console.log(response)
+
+
+    if (!response.ok) {
+        alert("Lo sentimos, reintente más tarde");
+        throw new Error()
+    }
+    } catch (error) {
+        console.log(error.message);
+    };
+
+
+
+};
+
